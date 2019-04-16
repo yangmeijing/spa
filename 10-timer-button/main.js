e20c7f46de629109577ebaaa37506d80a0866796
yangmeijing <1806102644@qq.com>
2019-04-16 03:09:32 +0000

    publish wet site

---
+++ 10-timer-button/main.js
@@ -0,0 +1,1 @@
+$(function(){var t=6,n=$('input[type="button"]'),e=window.setInterval(function(){t--,n.val("同意("+t+"s)"),0==t&&(window.clearInterval(e),n.removeAttr("disabled"),n.val("同意"))},1e3);n.click(function(){alert("SPA!")})});
